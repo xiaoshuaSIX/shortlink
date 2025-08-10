5869210a8a7ebe8e50cab1007367902556ad05c5
zhaoshuai <xs2357377358@163.com>
2025-08-10 12:23:16 +0800

    feature: 开发短链接后管系统相关功能

---
+++ admin/src/main/java/com/xiaoshuai66/shortlink/admin/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.xiaoshuai66.shortlink.admin.controller;
+
+import com.xiaoshuai66.shortlink.admin.common.convention.result.Result;
+import com.xiaoshuai66.shortlink.admin.common.convention.result.Results;
+import com.xiaoshuai66.shortlink.admin.dto.resp.UserRespDTO;
+import com.xiaoshuai66.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ *
+ * @Author 赵帅
+ * @Create 7/27/25 PM2:28
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 根据用户名查询用户信息
+     */
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        return Results.success(userService.getUserByUsername(username));
+    }
+
+}
---
+++ admin/src/main/java/com/xiaoshuai66/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xiaoshuai66.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaoshuai66.shortlink.admin.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+ *
+ * @Author 赵帅
+ * @Create 8/10/25 AM12:03
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}

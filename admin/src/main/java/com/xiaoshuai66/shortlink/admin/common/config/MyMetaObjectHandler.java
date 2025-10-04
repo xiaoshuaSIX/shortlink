bd2d0ae1135fbc7b44aff8dcd95b357825b3b5f1
zhaoshuai <xs2357377358@163.com>
2025-10-04 17:54:45 +0800

    feature: 增加 MyBatis-Plus 元数据自动填充配置

---
+++ admin/src/main/java/com/xiaoshuai66/shortlink/admin/common/config/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.xiaoshuai66.shortlink.admin.common.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * MyBatis-Plus 原数据自动填充类
+ */
+@Primary
+@Component(value = "myMetaObjectHandlerByAdmin")
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
